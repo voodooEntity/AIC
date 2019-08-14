657a780f1246572fdbf79a9c3217e17656b1a167
foobarrbaz <foo@bar.com>
2019-08-14 23:35:43 +0200

    updating to do HSV distance instead of RGB distance

--- AIC.php
+++ AIC.php
@@ -11,6 +11,9 @@ class AnalyzeImageColors {
     private $imageRes;
     private $imageWith;
     private $imageHeight;
+    private $intSaturationWeight = 4;
+    private $intValueWeight      = 3;
+    private $intHueWeight        = 10;
     private $arrColors   = [];
     private $colorCounts = [];
     
@@ -33,21 +36,6 @@ class AnalyzeImageColors {
         return false;
     }
     
-    public function colorDiff($alpha,$beta) {
-        // do the math on each tuple
-        // could use bitwise operates more efficiently but just do strings for now.
-        // alpha
-        $red1   = hexdec(substr($alpha,0,2));
-        $green1 = hexdec(substr($alpha,2,2));
-        $blue1  = hexdec(substr($alpha,4,2));
-        // beta
-        $red2   = hexdec(substr($beta,0,2));
-        $green2 = hexdec(substr($beta,2,2));
-        $blue2  = hexdec(substr($beta,4,2));
-    
-        return abs($red1 - $red2) + abs($green1 - $green2) + abs($blue1 - $blue2) ;
-    }
-    
     public function process($filePath) {
         // first we load the image
         if(!$this->loadImage($filePath)) {
@@ -60,7 +48,7 @@ class AnalyzeImageColors {
             for($y = 0 ; $y < $this->imageHeight ; $y++) {
                 // get the closes color to our given array of colors
                 // based on the configured maxc distance
-                $color = $this->findClosestColorInList($x,$y);
+                $color = $this->findClosestColorInListByHsvDistance($x,$y);
                 // if a color could be found
                 if(AnalyzeImageColors::ERR_NO_HITS !== $color) {
                     $this->colorCounts[$color["name"]]++;
@@ -71,19 +59,30 @@ class AnalyzeImageColors {
         return $this->colorCounts;
     }
     
-    private function findClosestColorInList($x,$y) {
-        // reset min distance to allowed max
-        $smallestDistance = $this->intMaxDistance;
+    private function findClosestColorInListByHsvDistance($x,$y) {
         $closestColor     = false;
         // calculate the hex for the pixel we have to test
-        $testHex          = $this->getPixelColorHex($x,$y);
+        $testRgba         = $this->getPixelColor($x,$y);
         // filter full alpha
-        if($testHex === AnalyzeImageColors::SKIP_ALPHA) {
+        if($testRgba === AnalyzeImageColors::SKIP_ALPHA) {
             return AnalyzeImageColors::ERR_NO_HITS;
         }
         // test all registered colors for closest distance
-        foreach($this->arrColors as $colorName => $colorHex) {
-            $currDist = $this->colorDiff($colorHex,$testHex);
+        foreach($this->arrColors as $colorName => $colorData) {
+            // reset min distance to allowed max or
+            // if we got a specific precision we use it
+            $smallestDistance = $this->intMaxDistance;
+            if(isset($colorData["precision"])) {
+                $smallestDistance = $colorData["precision"];
+            }
+            // get the color diffrence bei hsv
+            $currDist = $this->colorDiffByHsvDistance(
+                $this->hexToRgb($colorData["hex"]),
+                $testRgba
+            );
+            if($colorName == "braun") {
+                //echo "|" . $currDist;
+            }
             // check if the color is closer than all before
             if($currDist < $smallestDistance) {
                 // seems to be, safe it for return
@@ -94,11 +93,56 @@ class AnalyzeImageColors {
         if(false === $closestColor) {
             return AnalyzeImageColors::ERR_NO_HITS;
         }
-        //var_dump($smallestDistance);
         return [
             "name"     => $closestColor,
             "distance" => $smallestDistance
         ];
+    }
+    
+    private function colorDiffByHsvDistance($alpha,$beta) {
+        // get the hsv values
+        $hsvAlpha = $this->rgbToHsv($alpha["red"], $alpha["green"], $alpha["blue"]);
+        $hsvBeta  = $this->rgbToHsv($beta["red"], $beta["green"], $beta["blue"]); 
+        // first we calc the hue difference, the strongest factor
+        $hueDistance   = $this->calcHueDistance($hsvAlpha["h"], $hsvBeta["h"]);
+        // second the saturation distance 
+        $satuDistance  = $this->calcSaturationDistance($hsvAlpha["s"], $hsvBeta["s"]);
+        // and third and final the brightness
+        $valueDistance = $this->calcValueDistance($hsvAlpha["v"], $hsvBeta["v"]);
+        // finally we add up and return
+        return $hueDistance + $satuDistance + $valueDistance;
+    }
+    
+    private function calcHueDistance($alpha, $beta) {
+        $arr  = [$alpha,$beta];
+        asort($arr);
+        $factor = $arr[1] - $arr[0];
+        if($arr[0] < 10 && $arr[1] > 90) {
+            $factor = 100 - $arr[1] + $arr[0];
+            //echo $factor . "|";
+        }
+        return  $this->intHueWeight / 30 * $factor;
+    }
+    
+    private function calcSaturationDistance($alpha, $beta) {
+        $arr  = [$alpha,$beta];
+        asort($arr);
+        return $this->intSaturationWeight / 100 * ($arr[1] - $arr[0]) ;
+    }
+    
+    private function calcValueDistance($alpha, $beta) {
+        $arr  = [$alpha,$beta];
+        asort($arr);
+        return $this->intValueWeight / 100 * ($arr[1] - $arr[0]) ;
+    }
+    
+    private function getPixelColor($x, $y) {
+        $rgb    = imagecolorat($this->imageRes,$x,$y);
+        $colors = imagecolorsforindex($this->imageRes, $rgb);
+        if($colors["alpha"] != 127) {
+            return $colors;
+        }
+        return AnalyzeImageColors::SKIP_ALPHA;
     }
     
     private function getPixelColorHex($x,$y) {
@@ -106,13 +150,19 @@ class AnalyzeImageColors {
         $colors = imagecolorsforindex($this->imageRes, $rgb);
         if($colors["alpha"] != 127) {
             $hex    = sprintf("%02x%02x%02x", $colors["red"], $colors["green"], $colors["blue"]);
-            //var_dump($colors);
-            //var_dump($hex);
             return $hex;            
         }
         return AnalyzeImageColors::SKIP_ALPHA;
     }
     
+    public function hexToRgb($hex) {
+        $split = str_split($hex, 2);
+        $r = hexdec($split[0]);
+        $g = hexdec($split[1]);
+        $b = hexdec($split[2]);
+        return ["red" => $r, "green" => $g, "blue" => $b];
+    }
+
     private function calcImageReso() {
         $this->imageHeight = imagesy($this->imageRes);
         $this->imageWidth  = imagesx($this->imageRes);
@@ -125,33 +175,123 @@ class AnalyzeImageColors {
         ];
     }
     
+    function rgbToHsv($r, $g, $b) {
+        $r = $r / 255;
+        $g = $g / 255;
+        $b = $b / 255;
+        $v = max($r, $g, $b);
+        $diff  = $v - min($r, $g, $b);
+        $diffc = function($c) use ($v, $diff) {
+            return ($v - $c) / 6 / $diff + 1 / 2;
+        };
+        if($diff == 0) {
+            $h = $s = 0;
+        } else {
+            $s  = $diff / $v;
+            $rr = $diffc($r);
+            $gg = $diffc($g);
+            $bb = $diffc($b);
+            if($r === $v) {
+                $h = $bb - $gg;
+            } else if($g === $v) {
+                $h = (1 / 3) + $rr - $bb;
+            } else if($b === $v) {
+                $h = (2 / 3) + $gg - $rr;
+            }
+            if($h < 0) {
+                $h += 1;
+            } else if($h > 1) {
+                $h -= 1;
+            }
+        }
+        return ["h" => round($h * 100), "s" => round($s * 100), "v" => round($v * 100) ];
+        //return ["h" => round($h * 360), "s" => round($s * 100), "v" => round($v * 100) ];
+    }
+    
 }
-$imagePath = "824304416009-06.png";
+$imagePath = "test.png";
 $arrColors = [
-    "beige" => "d1bc8a",
-    "blau" => "0000ff",
-    "braun" => "783000",
-    "coral" => "f88379",
-    "gelb" => "ffff00",
-    "gold" => "d4af37",
-    "grau" => "808080",
-    "grün" => "33ff33", // 006400
-    "lachs" => "f07030",
-    "lila" => "800080",
-    "oliv" => "bab86c",
-    "orange" => "ffa500",
-    "pink" => "ffc0cb",
-    "rosa" => "ea899a",
-    "rot" => "ff0000",
-    "schwarz" => "000000",
-    "silber" => "c0c0c0",
-    "türkis" => "48d1cc",
-    "weiß" => "ffffff"
+    "beige" => [
+        "hex"       => "d1bc8a",
+        "precision" => 10
+    ],
+    "blau" => [
+        "hex"       => "0000ff",
+        "precision" => 15
+    ],
+    "braun" => [
+        "hex"       => "783000",
+        "precision" => 17
+    ],
+    "coral" => [
+        "hex"       => "f88379",
+        "precision" => 10
+    ],
+    "gelb" => [
+        "hex"       => "ffff00",
+        "precision" => 10
+    ],
+    "gold" => [
+        "hex"       => "d4af37",
+        "precision" => 8
+    ],
+    "grau" => [
+        "hex"       => "808080",
+        "precision" => 10
+    ],
+    "grün" => [
+        "hex"       => "33ff33",
+        "precision" => 15
+    ],
+    "lachs" => [
+        "hex"       => "f07030",
+        "precision" => 8
+    ],
+    "lila"    => [
+        "hex"       => "800080",
+        "precision" => 8
+    ],
+    "oliv"    => [
+        "hex"       => "bab86c",
+        "precision" => 8
+    ],
+    "orange"  => [
+        "hex"       => "ffa500",
+        "precision" => 10
+    ],
+    "pink"    => [
+        "hex"       => "ffc0cb",
+        "precision" => 9
+    ],
+    "rosa"    => [
+        "hex"       => "ea899a",
+        "precision" => 9
+    ],
+    "rot"     => [
+        "hex"       => "ff0000",
+        "precision" => 15
+    ],
+    "schwarz" => [
+        "hex"       => "000000",
+        "precision" => 7
+    ],
+    "silber"  => [
+        "hex"       => "c0c0c0",
+        "precision" => 9
+    ],
+    "türkis"  => [
+        "hex"       => "48d1cc",
+        "precision" => 7
+    ],
+    "weiß"    => [
+        "hex"       => "ffffff",
+        "precision" => 7
+    ],
 ];
 
 
 // test
-$objAIC  = new AnalyzeImageColors(70);
+$objAIC  = new AnalyzeImageColors(15);
 $objAIC->setColors($arrColors);
 
 
